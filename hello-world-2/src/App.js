18f6c10eafd5156afc1c2dd40b4fbb7bcb157235
Ese <eomene2@uic.edu>
2020-08-25 23:03:27 -0500

    making new hello world project

--- hello-world-2/src/App.js
+++ hello-world-2/src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          This is Ese Omene.
+          Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
